19708d6a6e83da23cb710591b09413ddc956da72
proud-p <avika.pulges@hogarth.com>
2025-07-10 12:57:22 +0100

    9:30 ep 2 Add parser and AST node classes for BASIC interpreter
    Introduces NumberNode and BinOpNode classes to represent AST nodes, and implements a Parser class to parse tokens into an abstract syntax tree. Updates the run function to return the AST instead of tokens. Adds a grammar.txt file describing the supported grammar rules.

--- BASIC_interpreter_language_tutorial-main/basic.py
+++ BASIC_interpreter_language_tutorial-main/basic.py
@@ -152,7 +152,77 @@ In this case, it will take a string of BASIC code and break it down into tokens 
             return Token(TT_FLOAT, float(num_str))
         
 
+#################################################
+# NODES 
+#################################################
 
+class NumberNode:
+    def __init__(self,token):
+        self.token = token
+    def __repr__(self): #return a string containing a printable representation of an object
+        return f"{self.token}"
+    
+class BinOpNode:
+    def __init__(self,left_node,op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+        
+    def __repr__(self):
+        return f"({self.left_node}, {self.op_tok}, {self.right_node})"
+    
+    
+
+#################################################
+# PARSER
+#################################################
+
+class Parser:
+    def __init__(self,tokens):
+        self.tokens = tokens
+        self.tok_idx = -1 #token index
+        self.advance()
+        
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok 
+    
+    def parse(self): #call expression - advance till find plus or minus
+        res = self.expr()
+        return res
+        
+    def factor(self): #if token is int or float advance and return number node class
+        tok = self.current_tok
+        
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok) #recursive functions since call eachother
+    
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+        
+    
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+    
+    
+    def bin_op(self, func, ops): # pass in rule and accepted operations
+        # since term and expression is basically the same with the difference being it's either doing the same operation on a factor or a term, we can create a code that can be used for both 
+        #scenarios
+        
+        left =  func()
+        
+        while self.current_tok.type in ops: #if current tok is operation token
+            op_tok = self.current_tok
+            self.advance() #advance to next
+            right = func()
+            left = BinOpNode(left,op_tok,right)
+            
+        return left #now a binary operation node because we re-assigned it - becomes a term
+        
+        
 
 #################################################
 # RUN
@@ -162,4 +232,10 @@ def run(fn,text):
     lexer = Lexer(fn,text)
     tokens, error = lexer.make_tokens()
     
-    return tokens, error
+    if error: return None, error
+    
+    # Generate Abstract Syntax Tree
+    parser = Parser(tokens)
+    ast = parser.parse()
+    
+    return ast, None
